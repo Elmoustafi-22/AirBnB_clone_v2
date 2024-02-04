f744d959eac4baef522ccc3387e7ab35997b1c82
root <mustophaqadir@gmail.com>
2024-02-04 10:30:22 -0800

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -43,3 +43,11 @@ def do_deploy(archive_path):
         return True
     except:
         return False
+
+
+def deploy():
+    """Creates and distributes an archive to the web server"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
