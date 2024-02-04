daae406a41f7f2701b0b9410aab5be4ac7bd0b7a
root <mustophaqadir@gmail.com>
2024-02-04 01:55:09 -0800

    corrected

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,7 +10,6 @@ sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo touch /data/web_static/releases/test/index.html
-sudo ln -s -f /data/web_static/releases/test /data/web_static/current
 sudo echo "<html>
   <head>
   </head>
@@ -18,6 +17,11 @@ sudo echo "<html>
     Holberton School
   </body>
 </html>" | sudo tee sudo tee /data/web_static/releases/test/index.html
+
+sudo ln -s -f /data/web_static/releases/test /data/web_static/current
+
 sudo chown -R ubuntu:ubuntu /data/
+
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
 sudo service nginx restart
